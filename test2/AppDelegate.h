1c5cfc485bba36320435b4fd29e0d9af6de853c7
  MR.dream <673646979@qq.com>
2018-04-05 10:03:45 +0800

    Initial Commit

---
+++ test2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test2
+//
+//  Created by dreamforstar on 2018/4/5.
+//  Copyright © 2018年 com.jiankeyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  test2
+//
+//  Created by dreamforstar on 2018/4/5.
+//  Copyright © 2018年 com.jiankeyan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
